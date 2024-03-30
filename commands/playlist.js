7d8dc5026d295bf3e277f4168c43c08799711598
MusicBot111 <165457169+MusicBot111@users.noreply.github.com>
2024-03-30 17:17:48 +0800

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -214,7 +214,7 @@ await interaction.editReply({
           .setAuthor({
             name: 'Album Deleted Sucessfully',
             iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1215554404527116288/7762-verified-blue.gif',
-            url: 'https://discord.gg/FUEHs7RCqz'
+            url: 'https://discord.gg/gmmfcDFbqw'
           })
   .setDescription(`Hey <@${interaction.member.id}>, your album has been Deleted successfully! ✨`)
   .setTimestamp();
@@ -255,7 +255,7 @@ await interaction.editReply({
        .setAuthor({
           name: 'Song Added to Your Album',
           iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1213430944007061574/6943_Verified.gif',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/gmmfcDFbqw'
         })
         .setDescription(`Hey <@${interaction.member.id}>, your song has been Added successfully! ✨`)
         .setFooter({ text: 'TUKMOL' })
@@ -324,7 +324,7 @@ await interaction.editReply({
           .setAuthor({
             name: 'Song Removed Sucessfully',
             iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1215554404527116288/7762-verified-blue.gif',
-            url: 'https://discord.gg/FUEHs7RCqz'
+            url: 'https://discord.gg/gmmfcDFbqw'
           })
   .setDescription(`Hey <@${interaction.member.id}>, your song has been Removed successfully! ✨`)
   .setTimestamp();
@@ -404,7 +404,7 @@ await interaction.editReply({
            .setAuthor({
           name: 'Album Songs',
           iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1213422313035407360/8218-alert.gif',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/gmmfcDFbqw'
         })
             .setThumbnail(interaction.user.displayAvatarURL({ size: 2048, dynamic: true }))
             .setColor(client.config.embedColor) 
@@ -501,13 +501,13 @@ await interaction.editReply({
           .setAuthor({
             name: 'Your Albums',
             iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1213422313035407360/8218-alert.gif',
-            url: 'https://discord.gg/FUEHs7RCqz'
+            url: 'https://discord.gg/gmmfcDFbqw'
           })
           .setColor(client.config.embedColor)
           .setDescription(`\n${playlist?.playlist?.map(data =>
             `\n**${number++} |** \`${data.name}\` - **${playlist?.musics?.filter(m => m.playlist_name === data.name)?.length || 0}** plays (<t:${Math.floor(data.createdTime / 1000) }:R>)`
           ) }`)
-          .setFooter({ text: 'YouTube - RTX GAMING' })
+          .setFooter({ text: 'Tukmol' })
         return interaction.reply({ embeds: [embed] }).catch(e => { }) 
 
       }
@@ -565,7 +565,7 @@ await interaction.editReply({
             .setAuthor({
               name: 'Top Albums',
               iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1213422313035407360/8218-alert.gif',
-              url: 'https://discord.gg/FUEHs7RCqz'
+              url: 'https://discord.gg/gmmfcDFbqw'
             })
             .setThumbnail(interaction.user.displayAvatarURL({ size: 2048, dynamic: true }))
             .setColor(client.config.embedColor)
@@ -644,7 +644,7 @@ await interaction.editReply({
               .setAuthor({
           name: 'Top Albums',
           iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1213422313035407360/8218-alert.gif',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/gmmfcDFbqw'
         })
               .setThumbnail(interaction.user.displayAvatarURL({ size: 2048, dynamic: true }))
               .setColor(client.config.embedColor)
